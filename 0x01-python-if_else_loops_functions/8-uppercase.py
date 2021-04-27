5e0e73aef3c0834686b5da3fc9a4c5bd39f14ac3
Naji Kadri <3214@holbertonschool.com>
2021-04-27 17:55:41 +0000

    Fix Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(''.join([chr(ord(c) - 32) if ord(c) >=
-          92 and ord(c) <= 122 else c for c in str]))
+    text = ""
+    for letter in str:
+        if ord(letter) >= 92 and ord(letter) <= 122:
+            text += chr(ord(letter)-32)
+        else:
+            text += letter
+    print(text)
