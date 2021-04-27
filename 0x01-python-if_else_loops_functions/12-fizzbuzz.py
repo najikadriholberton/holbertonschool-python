6a20bfaa2b7eb9f39cb63d81093a23f9f0bea3cb
Naji Kadri <3214@holbertonschool.com>
2021-04-27 18:16:27 +0000

    Python 0x01 Part 2

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1,101):
+        displayed = False
+        if i % 3 == 0:
+            print("Fizz", end="")
+            displayed = True
+        if i % 5 == 0:
+            print("Buzz", end="")
+            displayed = True
+        if not displayed:
+            print(i, end="")
+        print(" ", end="")
