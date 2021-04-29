8417d2d1d5826e573d891badc339ffc6072b7143
Naji Kadri <3214@holbertonschool.com>
2021-04-29 16:14:06 +0000

    Args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+n = len(argv) - 1
+
+if n == 0:
+    print("0 arguments.")
+elif n == 1:
+    print("1 argument:")
+    print("1: %s" % argv[1])
+else:
+    print("%d arguments:" % n)
+    for i in range(1, n+1):
+        print("%d: %s" % (i, argv[i]))
