1abc2849e517c2c683f9b8196121a3b46302cd61
Naji Kadri <3214@holbertonschool.com>
2021-04-29 15:46:51 +0000

    INIT Python 0x02

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+adder = __import__('add_0')
+a=1
+b=2
+print("{} + {} = {}".format(a,b, adder.add(a,b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+# 0x02-python-import_modules
+
+Learn how to import modules in Python
