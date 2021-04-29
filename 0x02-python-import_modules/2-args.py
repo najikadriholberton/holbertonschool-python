21f465a44aa8305ccf54c4989916eaade804e3e4
Naji Kadri <3214@holbertonschool.com>
2021-04-29 16:17:51 +0000

    Fix Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
 
-n = len(argv) - 1
+if __name__ == '__main__':
 
-if n == 0:
-    print("0 arguments.")
-elif n == 1:
-    print("1 argument:")
-    print("1: %s" % argv[1])
-else:
-    print("%d arguments:" % n)
-    for i in range(1, n+1):
-        print("%d: %s" % (i, argv[i]))
+    n = len(argv) - 1
+
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+        print("1: %s" % argv[1])
+    else:
+        print("%d arguments:" % n)
+        for i in range(1, n+1):
+            print("%d: %s" % (i, argv[i]))
