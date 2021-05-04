1039c015f90b7eacacced333fcd7862866ec6015
Naji Kadri <3214@holbertonschool.com>
2021-05-04 18:56:46 +0000

    Fix PEP8 Roman To Int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_number):
-    roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_val = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000}
     int_val = 0
     if isinstance(roman_number, str) and roman_number is not None:
         for i in range(len(roman_number)):
