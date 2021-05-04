27570dad2481a9620fbc947ba66b17d50c5035eb
Naji Kadri <3214@holbertonschool.com>
2021-05-04 18:52:38 +0000

    More DS Part 3

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_number):
+    roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    if isinstance(roman_number, str) and roman_number is not None:
+        for i in range(len(roman_number)):
+            comp1 = roman_number[i - 1]
+            if i > 0 and roman_val[roman_number[i]] > roman_val[comp1]:
+                int_val += roman_val[roman_number[i]] - 2 * roman_val[comp1]
+            else:
+                int_val += roman_val[roman_number[i]]
+        return int_val
+    return 0
