ecf9d7de960520653e7e79d4fbccf185215c3199
Naji Kadri <3214@holbertonschool.com>
2021-05-04 18:10:54 +0000

    Fix Square Matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     n = len(matrix)
-    m = len(matrix)
+    m = len(matrix[0])
     mat = []
     for i in range(n):
         mat.append([])
