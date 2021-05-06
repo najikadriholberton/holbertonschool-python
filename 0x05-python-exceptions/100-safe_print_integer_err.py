21019b17c4a3a67c0bb13e57449554d09b584b08
Naji Kadri <3214@holbertonschool.com>
2021-05-06 11:33:01 +0000

    Update Safe Err Print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
-        sys.stderr.write("Exception: " + str(e))
+    except Exception as e:
+        sys.stderr.write("Exception: " + str(e) + "\n")
         return False
