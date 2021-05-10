4a12f71bc220685ada5f9fa2d1c33315931b9f4b
Naji Kadri <3214@holbertonschool.com>
2021-05-10 17:35:16 +0000

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A module that has a class Square"""
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        self.__size = val
+
+        size = self.__size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        """print the square using # character"""
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if self.__size == 0:
+            print()
