6d09fcc73903918d1cd12b3a22b6bc73b3902c86
Naji Kadri <3214@holbertonschool.com>
2021-05-11 23:33:34 +0000

    Add docstring for Say Name Function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """Say My Name Function"""
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     elif type(last_name) != str:
