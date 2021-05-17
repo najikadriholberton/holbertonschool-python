da59be6695193472b3c49b8c8cf612aac14cfad4
Naji Kadri <3214@holbertonschool.com>
2021-05-17 17:45:52 +0000

    Fix Wrong Var 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,9 +14,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(width) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -26,8 +26,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(height) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
