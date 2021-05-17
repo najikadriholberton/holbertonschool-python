0b2c12bf89d4247510d4dfe75d2ceec5b2b749e5
Naji Kadri <3214@holbertonschool.com>
2021-05-17 18:13:44 +0000

    String Repr

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Module about Rectangles"""
+
+
+class Rectangle:
+    """class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns perimeter of the rectangle"""
+        return 0 if self.__width == 0 or self.__height == 0 else 2 * \
+            (self.__width + self.__height)
+
+    def __str__(self):
+        """return string form of the class Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return '\n'.join(["#" * self.__width
+                              for _ in range(self.__height)])
