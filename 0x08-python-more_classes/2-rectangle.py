2b1c9006a189ba8e214b4f6d0a7190361bf90b17
Naji Kadri <3214@holbertonschool.com>
2021-05-17 17:53:52 +0000

    Fix Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """class Rectangle"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -14,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -26,7 +27,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -34,8 +35,9 @@ class Rectangle:
 
     def area(self):
         """returns area of the rectangle"""
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """returns perimeter of the rectangle"""
-        return 2 * (self.width + self.height)
+        return 0 if self.__width == 0 or self.__height == 0 else 2 * \
+            (self.__width + self.__height)
