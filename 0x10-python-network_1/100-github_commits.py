b4bb7c9abf5971a4d904dbfa38ee3e5cd21d73bd
Naji Kadri <3214@holbertonschool.com>
2021-06-07 20:26:37 +0000

    Update Github Commits

--- 0x10-python-network_1/100-github_commits.py
+++ 0x10-python-network_1/100-github_commits.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Latest 10 commits"""
-import requests
-from sys import argv
+"""
+module 100 commits
+"""
 
-if __name__ == '__main__':
-    github_commits = 'https://api.github.com/repos/{}/{}/commits'.format(
-        argv[1], argv[2])
-    r = requests.get(github_commits)
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    repo = argv[1]
+    user = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(user, repo)
+    r = requests.get(url)
     commits = r.json()
-    for i in commits[:10]:
+    for i in commits[0:10]:
         print(i.get('sha'), end=': ')
         print(i.get('commit').get('author').get('name'))
