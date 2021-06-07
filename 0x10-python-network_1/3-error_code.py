75dbdb0be1dcc8c0d4e7a3eebb60288c71081d8c
Naji Kadri <3214@holbertonschool.com>
2021-06-07 19:32:38 +0000

    Error Code #0:

---
+++ 0x10-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Error Code"""
+from urllib import request, error, parse
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = request.Request(argv[1])
+    try:
+        with request.urlopen(r) as response:
+            html = response.read()
+            print(html.decode('utf8'))
+        except error.HTTPError as e:
+            print("Error Code: " + str(e.code))
