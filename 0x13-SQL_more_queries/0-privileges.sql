a78fa0b31ff71f82b8f2311c4e370fb6351d4b28
Naji Kadri <3214@holbertonschool.com>
2021-06-11 19:32:04 +0000

    test o-priv

--- 0x13-SQL_more_queries/0-privileges.sql
+++ 0x13-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- this is a comment
+-- lists all privileges of the MySQL users user_0d_1
+-- lists all privileges of the MySQL users user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
