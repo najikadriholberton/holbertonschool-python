ec5a721cfe354087c6124551ab06df31027dbe40
Naji Kadri <3214@holbertonschool.com>
2021-06-14 20:12:55 +0000

    Fix NQueens

--- 0x14-nqueens/0-nqueens.py
+++ 0x14-nqueens/0-nqueens.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
-from sys import argv
+""""
+0-nqueens.py
+Module that solves the N Queens puzzle
+"""
 
-if len(argv) > 2:
-    print("Usage: nqueens N")
-    exit(1)
-try:
-    N = int(argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
+from sys import argv, exit
 
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
 
-def under_attack(col, queens):
-    return col in queens or \
-           any(abs(col - x) == len(queens)-i for i, x in enumerate(queens))
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
+    solution = []
 
-def solve(n):
-    solutions = [[]]
-    for row in range(n):
-        solutions = [solution+[i+1]
-                     for solution in solutions
-                     for i in range(BOARD_SIZE)
-                     if not under_attack(i+1, solution)]
-    return solutions
+    def solve_queens(row, n, solution):
+        if row == n:
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
 
-if __name__ == '__main__':
-    BOARD_SIZE = int(argv[1])
-    for answer in solve(BOARD_SIZE):
-        print(list([x-1, y-1] for x, y in enumerate(answer, start=1)))
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if queen[0] + queen[1] == placement[0] + placement[1]:
+                return False
+            if queen[0] - queen[1] == placement[0] - placement[1]:
+                return False
+        return True
+
+    solve_queens(0, n, solution)
