02280203b15d81d6e6911cee5dc4c78ec9d7b421
Naji Kadri <3214@holbertonschool.com>
2021-06-18 20:18:07 +0000

    update reddit 2

--- 0x16-count_it/0-count.py
+++ 0x16-count_it/0-count.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
-""" 0-count.py """
-from operator import itemgetter
+"""
+Recursive function that queries the reddit API,
+parses the title of all the hot articles,
+prints a sorted count of a given set of keywords
+"""
+
+from collections import Counter, defaultdict
+import re
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    agt = {"User-Agent": "linux:1:v2.1 (by /u/heimer_r)"}
-    payload = {"limit": "100", "after": after}
-    hot = requests.get(url, headers=agt, params=payload, allow_redirects=False)
-    if hot.status_code == 200:
-        posts = hot.json().get("data").get("children")
-        hot_list += [post.get("data").get("title") for post in posts]
-        after = hot.json().get("data").get("after")
-        if after is not None:
-            count_words(subreddit, word_list, hot_list, 1, after)
-        if init == 0:
-            hot_str = " ".join(hot_list)
-            hot_words = hot_str.split(" ")
-            word_list_low = sorted(word_list)
-            rt = []
-            for word in word_list_low:
-                num = len(
-                    list(
-                        filter(
-                            lambda hot_w: hot_w.lower() == word.lower(),
-                            hot_words)))
-                if num != 0:
-                    rt.append([word, num])
-            if len(rt) != 0:
-                i = 0
-                while i < len(rt) - 1:
-                    if rt[i + 1][0] is not None and rt[i][0] == rt[i + 1][0]:
-                        rt[i][1] += rt[i + 1][1]
-                        rt.pop(i + 1)
-                        rt.append([None, None])
-                        i -= 1
-                    i += 1
-                r = list(filter(lambda x: x != [None, None], rt))
-                r_sorted = sorted(r, key=lambda x: (x[1]), reverse=True)
-                for i in r_sorted:
-                    print(*i, sep=": ")
+def count_words(subreddit, word_list, res=defaultdict(int), after=None):
+    """Counting the words in Subreddit"""
+    agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132i\
+            Safari/537.36"
+    headers = {"User-Agent": agent}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if after:
+        url += '?after={}'.format(after)
+    try:
+        req = requests.get(url, headers=headers, allow_redirects=False).json()
+        titles = req.get('data').get('children')
+        for title in titles:
+            count = Counter(title.get('data').get('title').lower().split(' '))
+            for x in word_list:
+                if x.lower() in count:
+                    res[x] += count.get(x.lower())
+        after = req.get('data').get('after')
+        if after:
+            return count_words(subreddit, word_list, res, after)
+        sort_first = sorted(res.items(), key=lambda x: x[0])
+        for k, v in sorted(sort_first, key=lambda x: x[1], reverse=True):
+            print('{}: {}'.format(k, v))
+    except:
+        return
